0cb9df07111eaaaa9fbb6fe1f00a8cd4a4e9343b
Miles Le <eric.nguyen@fleishman.com>
2020-09-11 16:19:34 -0500

    Create tag migration

--- database/migrations/2020_09_10_185149_create_tags_table.php
+++ database/migrations/2020_09_10_185149_create_tags_table.php
@@ -14,8 +14,10 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
